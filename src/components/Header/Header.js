da1981bd16c478ced0bb57a84f8a8f2b6d9e3c4a
Aslam Hossain <54502570+engineer-aslam-hossain@users.noreply.github.com>
2021-12-30 19:24:44 +0600

    update downloadable link
    accepted

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -109,7 +109,7 @@ const Header = () => {
                 Contact
               </Link>
               <a
-                href="https://drive.google.com/uc?export=download&id=17sRDN4hhP0RKmVPJNkCYHaS8kGhlMYoA"
+                href="https://drive.google.com/uc?export=download&id=1n4sQowC4r8YCYr1uFvBlh6geYJrhXdcj"
                 target="blank"
                 className="resume"
               >
